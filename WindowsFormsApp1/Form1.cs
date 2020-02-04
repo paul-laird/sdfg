958638a87d8e89e9323dc71faa583e27e7189f1e
Paul Laird <89382@mydbs.ie>
2020-02-04 18:47:22 +0000

    ouch

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -21,5 +21,10 @@ namespace WindowsFormsApp1
         {
             MessageBox.Show("Hello");
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("ouch");
+        }
     }
 }
